1b9700f1193133b614876e21faee2cc4a320dfd9
José Victor Silva Santos <jose_victor_santos@hotmail.com>
2022-12-06 01:49:00 -0300

    Finalizado

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,12 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailAlreadyUsed = this.usersRepository.findByEmail(email);
+    if (emailAlreadyUsed) {
+      throw new Error("Email already used!");
+    }
+
+    return this.usersRepository.create({ name, email });
   }
 }
 
